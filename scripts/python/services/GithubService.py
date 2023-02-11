c7a21eb0165d1582c5fc87310fdfca3a1a79b1e6
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-02-11 14:07:22 +0000

    ✨ Add edge test cases to `get_outside_collaborators_login_names` tests

--- scripts/python/services/GithubService.py
+++ scripts/python/services/GithubService.py
@@ -10,5 +10,5 @@ class GithubService:
 
     def get_outside_collaborators_login_names(self) -> list[str]:
         logging.info("Getting Outside Collaborators Login Names")
-        outside_collaborators = self.client.get_organization(self.organisation_name).get_outside_collaborators()
+        outside_collaborators = self.client.get_organization(self.organisation_name).get_outside_collaborators() or []
         return [outside_collaborator.get("login") for outside_collaborator in outside_collaborators]
--- scripts/python/services/test_GithubService.py
+++ scripts/python/services/test_GithubService.py
@@ -3,44 +3,53 @@ from unittest.mock import call, MagicMock, patch
 
 from scripts.python.services.GithubService import GithubService
 
+ORGANISATION_NAME = "moj-analytical-services"
+
 
 class TestGithubService(unittest.TestCase):
+    mock_github = None
+
+    github_service = None
+
     def setUp(self):
-        print("setting up")
+        self.mock_github = MagicMock()
+        self.patcher = patch(target="github.Github.__new__", return_value=self.mock_github, autospec=True)
+        self.patcher.start()
+        self.addCleanup(self.patcher.stop)
+
+        self.github_service = GithubService("", ORGANISATION_NAME)
 
     def tearDown(self):
-        print("tearing down")
+        self.patcher.stop()
 
     def test_innit_sets_up_class(self):
-        mock_github = MagicMock()
-        patcher = patch(target="github.Github.__new__", return_value=mock_github, autospec=True)
-        patcher.start()
-        self.addCleanup(patcher.stop)
-
-        github_service = GithubService("", "moj-analytical-services")
-
-        self.assertIs(mock_github, github_service.client)
-        self.assertEqual("moj-analytical-services", github_service.organisation_name)
-
-        patcher.stop()
+        self.assertIs(self.mock_github, self.github_service.client)
+        self.assertEqual(ORGANISATION_NAME, self.github_service.organisation_name)
 
     def test_get_outside_collaborators_login_names_returns_login_names(self):
-        mock_github = MagicMock()
-        patcher = patch(target="github.Github.__new__", return_value=mock_github, autospec=True)
-        patcher.start()
-        self.addCleanup(patcher.stop)
-
-        mock_github.get_organization().get_outside_collaborators.return_value = [{"login": "tom-smith"},
-                                                                                 {"login": "john.smith"}]
-
-        github_service = GithubService("", "moj-analytical-services")
-
-        response = github_service.get_outside_collaborators_login_names()
+        self.mock_github.get_organization().get_outside_collaborators.return_value = [{"login": "tom-smith"},
+                                                                                      {"login": "john.smith"}]
+        response = self.github_service.get_outside_collaborators_login_names()
         self.assertEqual(["tom-smith", "john.smith"], response)
-        mock_github.get_organization.assert_has_calls(
-            [call(), call('moj-analytical-services'), call().get_outside_collaborators()])
 
-        patcher.stop()
+    def test_get_outside_collaborators_login_names_calls_downstream_services(self):
+        self.mock_github.get_organization().get_outside_collaborators.return_value = []
+        self.github_service.get_outside_collaborators_login_names()
+        self.mock_github.get_organization.assert_has_calls(
+            [call(), call(ORGANISATION_NAME), call().get_outside_collaborators()])
+
+    def test_get_outside_collaborators_login_names_returns_empty_list_when_collaborators_returns_none(self):
+        self.mock_github.get_organization().get_outside_collaborators.return_value = None
+        response = self.github_service.get_outside_collaborators_login_names()
+        self.assertEqual([], response)
+
+    def test_get_outside_collaborators_login_names_returns_exception_when_collaborators_returns_exception(self):
+        self.mock_github.get_organization().get_outside_collaborators.side_effect = ConnectionError
+        self.assertRaises(ConnectionError, self.github_service.get_outside_collaborators_login_names)
+
+    def test_get_outside_collaborators_login_names_returns_exception_when_organization_returns_exception(self):
+        self.mock_github.get_organization.side_effect = ConnectionError
+        self.assertRaises(ConnectionError, self.github_service.get_outside_collaborators_login_names)
 
 
 if __name__ == "__main__":
