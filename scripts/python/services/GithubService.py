e21abbf1788c2d3bb6305e967d78ead6b38ea9c5
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2023-02-13 15:49:38 +0000

    ✨ Add base `GithubService` Class (#18)

---
+++ scripts/python/services/GithubService.py
@@ -0,0 +1,15 @@
+import logging
+
+from github import Github
+
+
+class GithubService:
+    def __init__(self, org_token: str, organisation_name: str) -> None:
+        self.client = Github(org_token)
+        self.organisation_name = organisation_name
+
+    def get_outside_collaborators_login_names(self) -> list[str]:
+        logging.info("Getting Outside Collaborators Login Names")
+        outside_collaborators = self.client.get_organization(
+            self.organisation_name).get_outside_collaborators() or []
+        return [outside_collaborator.get("login") for outside_collaborator in outside_collaborators]
