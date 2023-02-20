74c278d84ed9366c1249cb19f3b8fd61ee2f1063
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-02-20 11:28:51 +0000

    ✨ Add smoke test to `archive repos` script

---
+++ scripts/python/test_archive_repositories.py
@@ -0,0 +1,19 @@
+import sys
+import unittest
+from unittest.mock import patch
+
+from github import Github
+
+from scripts.python import archive_repositories
+
+
+@patch.object(Github, "__new__")
+class TestArchiveRepositories(unittest.TestCase):
+
+    def test_main_smoke_test(self, mock1):
+        sys.argv = ["", "test"]
+        archive_repositories.main()
+
+
+if __name__ == "__main__":
+    unittest.main()
