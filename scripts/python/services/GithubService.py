00bab6f2ff2ad6ad8407e9a1f7dc5f9e096ea835
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-02-21 11:08:27 +0000

    ♻️ Refactor `close_expired_issues` into `GiithubService`

--- scripts/python/services/GithubService.py
+++ scripts/python/services/GithubService.py
@@ -1,6 +1,8 @@
 import logging
+from datetime import datetime, timedelta
 
 from github import Github
+from github.Issue import Issue
 
 
 class GithubService:
@@ -13,3 +15,17 @@ class GithubService:
         outside_collaborators = self.client.get_organization(
             self.organisation_name).get_outside_collaborators() or []
         return [outside_collaborator.get("login") for outside_collaborator in outside_collaborators]
+
+    def close_expired_issues(self, repository_name: str):
+        logging.info(f"Closing expired issues for {repository_name}")
+        issues = self.client.get_repo(f"{self.organisation_name}/{repository_name}").get_issues() or []
+        for issue in issues:
+            if self.__is_expired(issue):
+                issue.edit(state="closed")
+                logging.info(f"Closing issue in {repository_name}")
+
+    def __is_expired(self, issue: Issue):
+        grace_period = issue.created_at + timedelta(days=45)
+        return (issue.title == "User access removed, access is now via a team"
+                and issue.state == "open"
+                and grace_period < datetime.now())
