3e7ecdc0fdd0d16b232c2b894ccbb6be398228f4
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-02-21 20:18:54 +0000

    ♻️ Refactor `gql_client` into `GiithubService`

--- scripts/python/services/GithubService.py
+++ scripts/python/services/GithubService.py
@@ -4,24 +4,30 @@ from textwrap import dedent
 
 from github import Github
 from github.Issue import Issue
+from gql import Client
+from gql.transport.aiohttp import AIOHTTPTransport
 
 
 class GithubService:
     USER_ACCESS_REMOVED_ISSUE_TITLE: str = "User access removed, access is now via a team"
 
     def __init__(self, org_token: str, organisation_name: str) -> None:
-        self.client: Github = Github(org_token)
+        self.github_client_core_api: Github = Github(org_token)
+        self.github_client_gql_api: Client = Client(transport=AIOHTTPTransport(
+            url="https://api.github.com/graphql",
+            headers={"Authorization": f"Bearer {org_token}"},
+        ), fetch_schema_from_transport=False)
         self.organisation_name: str = organisation_name
 
     def get_outside_collaborators_login_names(self) -> list[str]:
         logging.info("Getting Outside Collaborators Login Names")
-        outside_collaborators = self.client.get_organization(
+        outside_collaborators = self.github_client_core_api.get_organization(
             self.organisation_name).get_outside_collaborators() or []
         return [outside_collaborator.login for outside_collaborator in outside_collaborators]
 
     def close_expired_issues(self, repository_name: str):
         logging.info(f"Closing expired issues for {repository_name}")
-        issues = self.client.get_repo(
+        issues = self.github_client_core_api.get_repo(
             f"{self.organisation_name}/{repository_name}").get_issues() or []
         for issue in issues:
             if self.__is_expired(issue):
@@ -37,7 +43,7 @@ class GithubService:
     def create_an_access_removed_issue_for_user_in_repository(self, user_name: str, repository_name: str):
         logging.info(
             f"Creating an access removed issue for user {user_name} in repository {repository_name}")
-        self.client.get_repo(f"{self.organisation_name}/{repository_name}").create_issue(
+        self.github_client_core_api.get_repo(f"{self.organisation_name}/{repository_name}").create_issue(
             title=self.USER_ACCESS_REMOVED_ISSUE_TITLE,
             assignee=user_name,
             body=dedent(f"""
@@ -58,5 +64,5 @@ class GithubService:
     def remove_user_from_repository(self, user_name: str, repository_name: str):
         logging.info(
             f"Removing user {user_name} from repository {repository_name}")
-        self.client.get_repo(
+        self.github_client_core_api.get_repo(
             f"{self.organisation_name}/{repository_name}").remove_from_collaborators(user_name)
--- scripts/python/services/test_GithubService.py
+++ scripts/python/services/test_GithubService.py
@@ -10,22 +10,28 @@ ORGANISATION_NAME = "moj-analytical-services"
 USER_ACCESS_REMOVED_ISSUE_TITLE = "User access removed, access is now via a team"
 
 
+@patch("gql.transport.aiohttp.AIOHTTPTransport.__new__", new=MagicMock)
+@patch("gql.Client.__new__")
 @patch("github.Github.__new__")
 class TestGithubServiceInit(unittest.TestCase):
 
-    def test_sets_up_class(self, mock_github):
-        mock_github.return_value = "test"
+    def test_sets_up_class(self, mock_github_client_core_api, mock_github_client_gql_api):
+        mock_github_client_core_api.return_value = "test_mock_github_client_core_api"
+        mock_github_client_gql_api.return_value = "test_mock_github_client_gql_api"
         github_service = GithubService("", ORGANISATION_NAME)
-        self.assertEqual("test", github_service.client)
+        self.assertEqual("test_mock_github_client_core_api", github_service.github_client_core_api)
+        self.assertEqual("test_mock_github_client_gql_api", github_service.github_client_gql_api)
         self.assertEqual(ORGANISATION_NAME,
                          github_service.organisation_name)
 
 
+@patch("gql.transport.aiohttp.AIOHTTPTransport.__new__", new=MagicMock)
+@patch("gql.Client.__new__", new=MagicMock)
 @patch("github.Github.__new__")
 class TestGithubServiceGetOutsideCollaborators(unittest.TestCase):
 
-    def test_returns_login_names(self, mock_github):
-        mock_github.return_value.get_organization().get_outside_collaborators.return_value = [
+    def test_returns_login_names(self, mock_github_client_core_api):
+        mock_github_client_core_api.return_value.get_organization().get_outside_collaborators.return_value = [
             Mock(NamedUser, login="tom-smith"),
             Mock(NamedUser, login="john.smith"),
         ]
@@ -33,35 +39,37 @@ class TestGithubServiceGetOutsideCollaborators(unittest.TestCase):
             "", ORGANISATION_NAME).get_outside_collaborators_login_names()
         self.assertEqual(["tom-smith", "john.smith"], response)
 
-    def test_calls_downstream_services(self, mock_github):
-        mock_github.return_value.get_organization(
+    def test_calls_downstream_services(self, mock_github_client_core_api):
+        mock_github_client_core_api.return_value.get_organization(
         ).get_outside_collaborators.return_value = []
         github_service = GithubService("", ORGANISATION_NAME)
         github_service.get_outside_collaborators_login_names()
-        github_service.client.get_organization.assert_has_calls(
+        github_service.github_client_core_api.get_organization.assert_has_calls(
             [call(), call(ORGANISATION_NAME), call().get_outside_collaborators()])
 
-    def test_returns_empty_list_when_collaborators_returns_none(self, mock_github):
-        mock_github.return_value.get_organization(
+    def test_returns_empty_list_when_collaborators_returns_none(self, mock_github_client_core_api):
+        mock_github_client_core_api.return_value.get_organization(
         ).get_outside_collaborators.return_value = None
         github_service = GithubService("", ORGANISATION_NAME)
         response = github_service.get_outside_collaborators_login_names()
         self.assertEqual([], response)
 
-    def test_returns_exception_when_collaborators_returns_exception(self, mock_github):
-        mock_github.return_value.get_organization(
+    def test_returns_exception_when_collaborators_returns_exception(self, mock_github_client_core_api):
+        mock_github_client_core_api.return_value.get_organization(
         ).get_outside_collaborators.side_effect = ConnectionError
         github_service = GithubService("", ORGANISATION_NAME)
         self.assertRaises(
             ConnectionError, github_service.get_outside_collaborators_login_names)
 
-    def test_returns_exception_when_organization_returns_exception(self, mock_github):
-        mock_github.return_value.get_organization.side_effect = ConnectionError
+    def test_returns_exception_when_organization_returns_exception(self, mock_github_client_core_api):
+        mock_github_client_core_api.return_value.get_organization.side_effect = ConnectionError
         github_service = GithubService("", ORGANISATION_NAME)
         self.assertRaises(
             ConnectionError, github_service.get_outside_collaborators_login_names)
 
 
+@patch("gql.transport.aiohttp.AIOHTTPTransport.__new__", new=MagicMock)
+@patch("gql.Client.__new__", new=MagicMock)
 @patch("github.Github.__new__")
 class TestGithubServiceCloseExpiredIssues(unittest.TestCase):
     DATE_BOUNDARY = 45
@@ -85,83 +93,85 @@ class TestGithubServiceCloseExpiredIssues(unittest.TestCase):
                          title=title or USER_ACCESS_REMOVED_ISSUE_TITLE,
                          state=state or self.ISSUE_STATE_CRITERIA)
 
-    def test_calls_downstream_services(self, mock_github):
+    def test_calls_downstream_services(self, mock_github_client_core_api):
         mock_issue = self.happy_path_base_issue_mock()
-        mock_github.return_value.get_repo().get_issues.return_value = [
+        mock_github_client_core_api.return_value.get_repo().get_issues.return_value = [
             mock_issue]
         github_service = GithubService("", ORGANISATION_NAME)
         github_service.close_expired_issues("test")
-        github_service.client.get_repo.assert_has_calls(
+        github_service.github_client_core_api.get_repo.assert_has_calls(
             [call(), call('moj-analytical-services/test'), call().get_issues()])
-        github_service.client.get_repo().get_issues.assert_has_calls(
+        github_service.github_client_core_api.get_repo().get_issues.assert_has_calls(
             [call()])
 
-    def test_sets_issues_to_closed_when_criteria_met(self, mock_github):
+    def test_sets_issues_to_closed_when_criteria_met(self, mock_github_client_core_api):
         mock_issue = self.happy_path_base_issue_mock()
-        mock_github.return_value.get_repo().get_issues.return_value = [
+        mock_github_client_core_api.return_value.get_repo().get_issues.return_value = [
             mock_issue]
         GithubService("", ORGANISATION_NAME).close_expired_issues("test")
         self.assertEqual(mock_issue.edit.call_args_list,
                          [call(state='closed')])
 
-    def test_sets_issues_to_closed_when_criteria_met_and_date_is_on_boundary(self, mock_github):
+    def test_sets_issues_to_closed_when_criteria_met_and_date_is_on_boundary(self, mock_github_client_core_api):
         mock_issue = self.happy_path_base_issue_mock(
             created_at=self.on_boundary_criteria)
-        mock_github.return_value.get_repo().get_issues.return_value = [
+        mock_github_client_core_api.return_value.get_repo().get_issues.return_value = [
             mock_issue]
         GithubService("", ORGANISATION_NAME).close_expired_issues("test")
         self.assertEqual(mock_issue.edit.call_args_list,
                          [call(state='closed')])
 
-    def test_does_not_edit_issue_when_title_criteria_not_met(self, mock_github):
+    def test_does_not_edit_issue_when_title_criteria_not_met(self, mock_github_client_core_api):
         mock_issue = self.happy_path_base_issue_mock(title="INCORRECT_TITLE")
-        mock_github.return_value.get_repo().get_issues.return_value = [
+        mock_github_client_core_api.return_value.get_repo().get_issues.return_value = [
             mock_issue]
         GithubService("", ORGANISATION_NAME).close_expired_issues("test")
         self.assertEqual(mock_issue.edit.call_args_list, [])
 
-    def test_does_not_edit_issue_when_state_criteria_not_met(self, mock_github):
+    def test_does_not_edit_issue_when_state_criteria_not_met(self, mock_github_client_core_api):
         mock_issue = self.happy_path_base_issue_mock(state="INCORRECT_STATE")
-        mock_github.return_value.get_repo().get_issues.return_value = [
+        mock_github_client_core_api.return_value.get_repo().get_issues.return_value = [
             mock_issue]
         GithubService("", ORGANISATION_NAME).close_expired_issues("test")
         self.assertEqual(mock_issue.edit.call_args_list, [])
 
-    def test_does_not_edit_issue_when_empty_list(self, mock_github):
+    def test_does_not_edit_issue_when_empty_list(self, mock_github_client_core_api):
         mock_issue = self.happy_path_base_issue_mock()
-        mock_github.return_value.get_repo().get_issues.return_value = []
+        mock_github_client_core_api.return_value.get_repo().get_issues.return_value = []
         GithubService("", ORGANISATION_NAME).close_expired_issues("test")
         self.assertEqual(mock_issue.edit.call_args_list, [])
 
-    def test_does_not_edit_issue_when_none_provided(self, mock_github):
+    def test_does_not_edit_issue_when_none_provided(self, mock_github_client_core_api):
         mock_issue = self.happy_path_base_issue_mock()
-        mock_github.return_value.get_repo().get_issues.return_value = None
+        mock_github_client_core_api.return_value.get_repo().get_issues.return_value = None
         GithubService("", ORGANISATION_NAME).close_expired_issues("test")
         self.assertEqual(mock_issue.edit.call_args_list, [])
 
-    def test_throws_exception_when_client_throws_exception(self, mock_github):
-        mock_github.return_value.get_repo = MagicMock(
+    def test_throws_exception_when_client_throws_exception(self, mock_github_client_core_api):
+        mock_github_client_core_api.return_value.get_repo = MagicMock(
             side_effect=ConnectionError)
         github_service = GithubService("", ORGANISATION_NAME)
         self.assertRaises(
             ConnectionError, github_service.close_expired_issues, "test")
 
 
+@patch("gql.transport.aiohttp.AIOHTTPTransport.__new__", new=MagicMock)
+@patch("gql.Client.__new__", new=MagicMock)
 @patch("github.Github.__new__")
 class TestGithubServiceCreateAnAccessRemovedIssueForUserInRepository(unittest.TestCase):
-    def test_calls_downstream_services(self, mock_github):
+    def test_calls_downstream_services(self, mock_github_client_core_api):
         github_service = GithubService("", ORGANISATION_NAME)
         github_service.create_an_access_removed_issue_for_user_in_repository(
             "test_user", "test_repository")
-        github_service.client.get_repo.assert_has_calls(
+        github_service.github_client_core_api.get_repo.assert_has_calls(
             [call('moj-analytical-services/test_repository'),
              call().create_issue(title=USER_ACCESS_REMOVED_ISSUE_TITLE, assignee='test_user',
                                  body='Hi there\n\nThe user test_user had Direct Member access to this repository and access via a team.\n\nAccess is now only via a team.\n\nYou may have less access it is dependant upon the teams access to the repo.\n\nIf you have any questions, please post in (#ask-operations-engineering)[https://mojdt.slack.com/archives/C01BUKJSZD4] on Slack.\n\nThis issue can be closed.')]
 
         )
 
-    def test_throws_exception_when_client_throws_exception(self, mock_github):
-        mock_github.return_value.get_repo = MagicMock(
+    def test_throws_exception_when_client_throws_exception(self, mock_github_client_core_api):
+        mock_github_client_core_api.return_value.get_repo = MagicMock(
             side_effect=ConnectionError)
         github_service = GithubService("", ORGANISATION_NAME)
         self.assertRaises(
@@ -169,19 +179,21 @@ class TestGithubServiceCreateAnAccessRemovedIssueForUserInRepository(unittest.Te
             "test_repository")
 
 
+@patch("gql.transport.aiohttp.AIOHTTPTransport.__new__", new=MagicMock)
+@patch("gql.Client.__new__", new=MagicMock)
 @patch("github.Github.__new__")
 class TestGithubServiceRemoveUserFromRepository(unittest.TestCase):
-    def test_calls_downstream_services(self, mock_github):
+    def test_calls_downstream_services(self, mock_github_client_core_api):
         github_service = GithubService("", ORGANISATION_NAME)
         github_service.remove_user_from_repository(
             "test_user", "test_repository")
-        github_service.client.get_repo.assert_has_calls([
+        github_service.github_client_core_api.get_repo.assert_has_calls([
             call('moj-analytical-services/test_repository'),
             call().remove_from_collaborators('test_user')
         ])
 
-    def test_throws_exception_when_client_throws_exception(self, mock_github):
-        mock_github.return_value.get_repo = MagicMock(
+    def test_throws_exception_when_client_throws_exception(self, mock_github_client_core_api):
+        mock_github_client_core_api.return_value.get_repo = MagicMock(
             side_effect=ConnectionError)
         github_service = GithubService("", ORGANISATION_NAME)
         self.assertRaises(
